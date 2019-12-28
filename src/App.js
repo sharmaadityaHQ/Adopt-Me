c7eeb27a82efa2befa49ea4a5217349740222b65
Aditya Sharma <sharmaaditya570191@gmail.com>
2019-12-28 21:40:51 +0530

    Add parcel bundler and extract Pet component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", { id: "something-important" }, [
@@ -27,4 +23,4 @@ const App = () => {
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
