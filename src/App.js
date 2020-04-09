7590beb2345a1a2814d89b9013d457958a092187
Aditya Sharma <sharmaaditya570191@gmail.com>
2020-04-09 10:10:51 +0530

    code splitting using lazy and Suspense

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, lazy, Suspense } from 'react'
 import { render } from 'react-dom'
 import SearchParams from './SearchParams'
 import { Router } from '@reach/router'
-import Details from './Details'
 import ThemeContext from './ThemeContext'
 import NavBar from './NavBar'
+
+const Details = lazy(() => import('./Details'))
 
 const App = () => {
   const themeHook = useState('darkblue')
@@ -12,10 +13,12 @@ const App = () => {
     <ThemeContext.Provider value={themeHook}>
       <div>
         <NavBar />
-        <Router>
-          <SearchParams path='/' />
-          <Details path='/details/:id' />
-        </Router>
+        <Suspense fallback={<h1>loading route ...</h1>}>
+          <Router>
+            <SearchParams path='/' />
+            <Details path='/details/:id' />
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   )
